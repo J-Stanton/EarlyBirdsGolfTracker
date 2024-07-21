c5e27388ac19041e898d0267e7da7fe4bd026b68
J-Stanton <jameskstanton123@gmail.com>
2024-07-21 19:54:49 +0200

    File creation

---
+++ pEarlyBirdsTracker.dpr
@@ -0,0 +1,14 @@
+program pEarlyBirdsTracker;
+
+uses
+  Vcl.Forms,
+  uEarlyBirdsTracker in 'uEarlyBirdsTracker.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
